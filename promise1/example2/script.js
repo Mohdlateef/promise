0078cedd36754d528bae9dad3b862c8aea0da10c
unknown <lateefahbaba123@gmail.com>
2024-01-29 22:27:26 +0530

    abc

---
+++ promise1/example2/script.js
@@ -0,0 +1,10 @@
+let btn=document.getElementById("btn");
+
+let output=document.getElementById("output");
+btn.addEventListener("click",()=>{
+	let text=document.getElementById("text").value;
+let num=document.getElementById("delay").value;
+	setTimeout(()=>{
+		output.innerText=`${text}`
+	},num)
+});
---
+++ promise1/exmaple/scriipt.js
@@ -0,0 +1,30 @@
+//your JS code here. If requi
+document.getElementById("btn").addEventListener("click",resolve);
+let age=document.getElementById("age");
+let name=document.getElementById("name");
+// alert(name.value);
+function resolve(e) {
+	e.preventDefault();
+	let b=name.value;
+	alert(b)
+	let a=age.value;
+	promise(a,b).then((data)=>{
+		alert(data);
+	}).catch((data)=>{alert(data)});
+	
+}
+	
+
+
+const promise=function(age,name){
+    let pr=new Promise((res,rej)=>{
+        if(age>18)
+        {
+            res(`Welcome ${name}. YOu can vote`);
+        }
+        else{
+            res(`Oh sorry ${name} you aren't old enough`);
+        }
+    })
+    return pr;
+}
